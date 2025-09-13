ddcf411eb5bc8a132f428081b706a49d39b5375f
Tanu Mourya <abhagakt236@gmail.com>
2025-09-13 13:32:05 +0530

    Add files via upload

---
+++ hollowpattern7.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main ()
+{
+    int n,i,j;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    for ( i = 1; i<= n; i++)
+    {
+        for (j = 1; j<= n; j++)
+        {
+        if ( i == j || i+j == 6 )
+         printf(" * ");
+        else
+         printf("   ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
