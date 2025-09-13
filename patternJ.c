1139d9b67f503b24f6699f284a18ad7a70f72272
Tanu Mourya <abhagakt236@gmail.com>
2025-09-13 13:42:23 +0530

    Add files via upload

---
+++ patternJ.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main ()
+{
+    int n,i,j;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    for ( i = 1; i<= n; i++)
+    {
+        for (j = 1; j<= n; j++)
+        {
+        if ( i == 1 || j ==(n/2)+1 && i <=n || j <= (n/2)+1 && i == n || j == 1 && i>=(n/2)+1)
+         printf(" * ");
+        else
+         printf("   ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
